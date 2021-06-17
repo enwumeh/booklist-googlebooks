f9ee96ec598e1ec52fd818248474874d10c5183a
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-06-16 21:27:52 -0500

    initial conditions accounted for

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const prompt = require('prompt');
 
 prompt.start();
+console.log('Welcome! What would you like to search for?')
 
 prompt.get(['option'], function (err, res) {
-  if (err) {
+  if (res.option == 'find book') {
+    //handle condition
+  }
+  else if (res.option == 'see list') {
+    //handle condition
+  }
+  else if (err) {
+    console.log("looks like this isn't one of the possible options!")
     return err;
   }
-  console.log(res.option)
 })
