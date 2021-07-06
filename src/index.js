2e60e488b87bb6b0e2c51fd130f7ecf72ef546a5
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-07-06 15:10:29 -0500

    one pusher yikes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const prompt = require("prompt");
-const axios = require("axios");
+// const prompt = require("prompt");
+import prompt from "prompt"
+// const axios = require("axios");
+// const lunch = require('./meals/lunch.js');
+import breakfast from './meals/breakfast.js'
+import lunch from './meals/lunch.js'
+import dinner from './meals/dinner.js'
+
 
 const initPrompt = () => {
   return console.log(
@@ -7,120 +13,22 @@ const initPrompt = () => {
   );
 };
 
-//main():  ask user what they'd like to do perform actions based on this
-const main = () => {
-  prompt.start();
-  initPrompt();
 
-  prompt.get(["option"], function (err, res) {
-    if (res.option == "books") {
-      getBooks();
-    } else if (res.option == "see list") {
-      viewBookList();
-    } else {
-      console.log("looks like this isn't one of the possible options!");
-      main();
-    }
-  });
-};
+console.log("tell us your order in this style: Brunch 2 1 3 1");
+prompt.get(["order"], function (err, res) {
+  let responseArr = res.order.split(" ");
+  let menu = responseArr[0];
 
-let booklist = [];
+  switch (menu) {
+    case "breakfast":
+      breakfast(responseArr);
+      break;
+    case "lunch":
+      lunch(responseArr);
 
-//Saves user book choice into booklist array
-const saveBook = (books) => {
-  console.log("select the book you want to save to your reading list (1-5)");
-  prompt.get(["bookNumber"], async function (err, res) {
-    let bookNumber = parseInt(res.bookNumber);
-
-    if (!bookNumber) {
-      console.log(
-        "\n" +
-          "sorry, this is not valid. You must type in a valid keyboard character" +
-          "\n" +
-          "\n"
-      );
-      main();
-    } else if (bookNumber < 1 || bookNumber > 5 || !isNaN(bookNumber)) {
-      console.log("\n" + "\n" + "number must be between 1 and 5");
-    } else {
-      let UserSelected = books[bookNumber - 1];
-      booklist.push(UserSelected);
-      console.log("The book you've saved is:");
-      console.log(UserSelected.volumeInfo.title);
-    }
-    main();
-  });
-  return booklist;
-};
-
-//searchResults() :fetch user data from API
-const searchResults = async (userBookInput) => {
-  console.log("You are looking for " + userBookInput);
-  const key = "AIzaSyDmYQmWIoPydQv0NzmBQTRS5G_w0wdNAis";
-  let fields = "items(id, volumeInfo(title,authors,publisher))";
-
-  const response = await axios.get(
-    "https://www.googleapis.com/books/v1/volumes?",
-    {
-      params: {
-        key: key,
-        q: userBookInput,
-        maxResults: 5,
-        fields: fields,
-      },
-    }
-  );
-
-  const books = response.data.items;
-  return books;
-};
-
-//getBooks() : prompt user for search choice, map through data
-const getBooks = async () => {
-  try {
-    console.log("\n" + "What would you like to search for?");
-
-    prompt.get(["findBooks"], async function (err, res) {
-      if (!res.findBooks) {
-        console.log(
-          "\n" +
-            "sorry, this is not valid. You must type in a valid keyboard character"
-        );
-
-        main();
-      } else {
-        const booksArr = await searchResults(res.findBooks);
-        console.log(booksArr);
-
-        booksArr.map((book, idx) => {
-          console.log(idx + 1);
-          console.log("Title:", book.volumeInfo.title);
-          console.log("Author: " + book.volumeInfo.authors);
-          console.log("Published by: " + book.volumeInfo.publisher + "\n");
-        });
-        saveBook(booksArr);
-      }
-    });
-  } catch (err) {
-    console.log(err);
+      break;
+    case "dinner":
+      dinner(responseArr);
+      break;
   }
-};
-//viewBookList() : shows books saved by user
-const viewBookList = () => {
-  if (booklist.length <= 0) {
-    console.log("Looks like there are no books in your reading list");
-    getBooks();
-  } else {
-    console.log("\n" + "Here are the entries in your list:");
-
-    let listing = booklist.map((entry, idx) => {
-      console.log("\n" + idx + 1, "Title: " + entry.volumeInfo.title);
-    });
-    main();
-  }
-  return listing;
-};
-
-main();
-
-(module.exports = main), viewBookList, getBooks, searchResults;
+});
---
+++ src/meals/breakfast.js
@@ -0,0 +1,55 @@
+export default function breakfast(order) {
+  let eggs = 0;
+  let toast = 0;
+  let coffee = 0;
+
+  order.forEach((num) => {
+    if (num == "1") {
+      eggs++;
+    } else if (num == "2") {
+      toast++;
+    } else if (num == "3") {
+      coffee++;
+    }
+  });
+  //drink: coffee
+  let drink;
+  if (coffee > 0) {
+    drink = "Water";
+  } else if ((coffee = 1)) {
+    drink = "Coffee";
+  } else {
+    drink = `Coffee x ${coffee}`;
+  }
+
+  //side: toast
+  let side;
+  if (toast > 1) {
+    side = `Toast(x${toast}`;
+  } else if ((toast <= 0)) {
+    side = null;
+    console.log("Unable to process: side must be ordered at least once");
+  } else {
+    side = "Chips";
+  }
+
+  //main: eggs
+  let main;
+  if (eggs > 1) {
+    main = `Eggs(${eggs})`;
+  } else if (eggs <= 0) {
+    console.log("Unable to process: Sandwich must be ordered exactly once");
+    main = null;
+  } else {
+    main = "Eggs";
+  }
+  let finalReturn;
+  if (eggs <= 0 || toast <= 0 ) {
+    finalReturn = null;
+  }
+  else {
+finalReturn = console.log(`${main},${side}, ${drink}`); 
+  }
+  
+  return finalReturn;
+}
